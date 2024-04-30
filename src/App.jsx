bf06a363b4bb52fd6b3dd2c985c92d6f6ed9d7d0
AValenzue <zuellagraphy@gmail.com>
2024-04-30 10:29:16 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>      {/* React div element */}
+      <Header/>
+      <h1>React Intro</h1>
+      <h2>Subtitle</h2>
+    </>
+  );
+}
+
+export default App;
